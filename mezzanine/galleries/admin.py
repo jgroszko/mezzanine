4127a479c8736cfb4719e9f9d2ab5ce89f37ef5a
John Groszko <john@tinythunk.com>
2013-06-25 23:35:39 -0500

    Add HTML5 upload progress on zip import

--- mezzanine/galleries/admin.py
+++ mezzanine/galleries/admin.py
@@ -7,17 +7,17 @@ from mezzanine.pages.admin import PageAdmin
 from mezzanine.galleries.models import Gallery, GalleryImage
 from mezzanine.utils.static import static_lazy as static
 
-
 class GalleryImageInline(TabularDynamicInlineAdmin):
     model = GalleryImage
 
-
 class GalleryAdmin(PageAdmin):
+
+    exclude = ['zip_import',]
 
     class Media:
         css = {"all": (static("mezzanine/css/admin/gallery.css"),)}
+        js = {"mezzanine/js/admin/gallery.js",}
 
     inlines = (GalleryImageInline,)
-
 
 admin.site.register(Gallery, GalleryAdmin)
